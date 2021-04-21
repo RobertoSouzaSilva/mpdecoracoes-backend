e71f0a0268560a88ca23bfe80e8da8f957d81f85
Roberto Souza <robertosouza57@yahoo.com.br>
2021-04-20 22:05:25 -0300

    projeto criado

---
+++ src/test/java/com/ts/mpdecoracoes/MpdecoracoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ts.mpdecoracoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpdecoracoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
